013f77a811ecd1b46cb72498878abc0ae37372b5
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-09-25 13:19:09 +1000

    Updated build

--- build.js
+++ build.js
@@ -12,12 +12,22 @@ ncc(path.resolve(__dirname, 'src', 'index.ts'), {
   await fs.mkdir(outDir, { recursive: true });
   const output = code.replaceAll('{VERSION}', packageObj.version);
   await fs.writeFile(path.resolve(outDir, 'index.js'), output);
-  await fs.writeFile(
-    path.resolve(outDir, 'LICENSE.txt'),
-    assets.LICENSE.source,
-  );
-  await fs.writeFile(
-    path.resolve(outDir, 'index.d.ts'),
-    assets['index.d.ts'].source,
-  );
+
+  Object.entries(assets)
+    .map(([key, value]) => ({
+      name: key.split('/').pop(),
+      dir: path.resolve(outDir, key.split('/').slice(0, -1).join('/')),
+      data: value.source,
+    }))
+    .filter(({ name }) => {
+      if (name.endsWith('.flf')) {
+        return name === 'Standard.flf';
+      } else {
+        return true;
+      }
+    })
+    .forEach(async ({ name, dir, data }) => {
+      await fs.mkdir(dir, { recursive: true });
+      await fs.writeFile(path.resolve(dir, name), data);
+    });
 });
