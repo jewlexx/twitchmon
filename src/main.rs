1efc3ee0edacd36f3e7ac610f59a5290c92c68f6
Juliette Cordor <grassojames5@gmail.com>
2022-03-12 16:21:05 +1100

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use twitchchat::{
+    connector::Connector, twitch::Capability::*, AsyncRunner, UserConfig, ANONYMOUS_LOGIN,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let (name, token) = ANONYMOUS_LOGIN;
+
+    let config = UserConfig {
+        name: name.into(),
+        token: token.into(),
+        capabilities: vec![Membership, Tags, Commands],
+    };
+
+    AsyncRunner::connect(, &config);
 }
